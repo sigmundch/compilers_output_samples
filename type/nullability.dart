539f11841d3dc429433827a2fd575c4b692a2b34
Mayank Patke <fishythefish@users.noreply.github.com>
2020-08-07 10:20:21 -0700

    Update nullability.dart

--- type/nullability.dart
+++ type/nullability.dart
@@ -1,17 +1,25 @@
-import 'legacy_lib.dart';
-
-main() {
-  String? s = null;
-
-  print(s is String?); // true
-  print(isLegacyString(s)); // false
-  print(s is String); // false
-  print(s is int?); // true
-
-  s = "Hello World";
-
-  print(s is String?); // true
-  print(s is String); // true
-  print(isLegacyString(s)); // true
-  print(s is int?); // false
-}
+import 'legacy_lib.dart';                                                           
+                                                                                    
+// Prevent dart2js from constant folding all the tests.                                                                                                       
+@pragma('dart2js:noInline')                                                         
+dynamic confuse(x) => x;                                                            
+dynamic s;                                                                          
+                                                                                    
+main() {                                                                            
+  s = confuse(null);                                                                
+                                                                                    
+  print(s is String?); // true                                                      
+  print(isLegacyString(s)); // false                                                
+  print(s is String); // false                                                      
+  print(s is int?); // true                                                         
+                                                                                    
+  s = confuse("Hello World");                                                       
+                                                                                    
+  print(s is String?); // true                                                      
+  print(s is String); // true                                                       
+  print(isLegacyString(s)); // true                                                 
+  print(s is int?); // false                                                        
+}                                                                                   
+                                                                                    
+// From legacy_lib.dart                                                             
+bool isLegacyString(dynamic s) => s is String;
